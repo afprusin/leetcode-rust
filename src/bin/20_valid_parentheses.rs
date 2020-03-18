ad4bf47fd9bc3e5fc93d4dd7ef4905cab91876b6
Andrew Frank Prusinowski <afprusin@gmail.com>
2020-03-18 12:55:53 -0500

    Revert accidental overwrite

--- src/bin/20_valid_parentheses.rs
+++ src/bin/20_valid_parentheses.rs
@@ -1,60 +1,26 @@
-use test::NamePadding::PadNone;
-
 pub struct Solution {}
-
-//Definition for singly-linked list.
-//#[derive(PartialEq, Eq, Clone, Debug)]
-pub struct ListNode {
-    pub val: i32,
-    pub next: Option<Box<ListNode>>
-}
-
-impl ListNode {
-    #[inline]
-    fn new(val: i32, next: Option<Box<ListNode>>) -> Self {
-        ListNode {
-            next,
-            val
-        }
-    }
-}
-
-pub struct LinkedList {
-    pub head: Option<Box<ListNode>>,
-    pub current: Option<Box<ListNode>>
-}
 
 impl Solution {
 
-    pub fn merge_two_lists(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        if l1.is_none() { return l2; }
-        else if l2.is_none() { return none };
+    pub fn is_valid(s: String) -> bool {
+        let brackets: Vec<char> = s.chars().collect();
+        let mut order_stack: Vec<char> = Vec::new();
 
-        let mut l1_iter = l1;
-        let mut l2_iter = l2;
-
-        let mut values: Vec<i32> = Vec::new();
-        while !l1_iter.is_none() || !l2_iter.is_none() {
-            if l1_iter.is_none() {
-                values.push(l2_iter.unwrap().val);
-            }
-            else if l2_iter.is_none() {
-                values.push(l1_iter.unwrap().val);
-            }
-            else if l1_iter.unwrap().val <= l2_iter.unwrap().val {
-                values.push(l1_iter.unwrap().val);
+        for bracket in brackets {
+            if bracket == '(' || bracket == '[' || bracket == '{' {
+                order_stack.push(bracket);
             }
             else {
-                values.push(l2_iter.unwrap().val);
+                match order_stack.pop() {
+                    None => { return false; },
+                    Some('(') => if bracket != ')' { return false; }
+                    Some('{') => if bracket != '}' { return false; }
+                    Some('[') => if bracket != ']' { return false; }
+                    _ => panic!("Invalid closing bracket")
+                };
             }
         }
-
-        let mut head_node = None;
-        for value in values {
-            head_node = Option::Some(Box::from(ListNode::new(value, head_node)));
-        }
-
-        return head_node;
+        return order_stack.is_empty();
     }
 
 }
