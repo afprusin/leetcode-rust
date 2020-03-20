bf52e5f4b92c80ba4f764b3072faa86b6454f3e9
Andrew Fentruck Prusinowski <andrew.prusinowski@backstopsolutions.com>
2020-03-20 12:05:51 -0500

    Add solution template

---
+++ src/bin/26_remove_duplicates_from_sorted_arrays.rs
@@ -0,0 +1,11 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+
+        }
+}
+
+fn main() {
+    let mut result = Solution::remove_duplicates(vec![1,1,2,3,4,4,4,4,5]);
+}
