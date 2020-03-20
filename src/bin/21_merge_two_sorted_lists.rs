d0c7f2907a9f53b14d46cb123832ddb428e81362
Andrew Fentruck Prusinowski <andrew.prusinowski@backstopsolutions.com>
2020-03-20 07:08:04 -0500

    Partially convert to if let

--- src/bin/21_merge_two_sorted_lists.rs
+++ src/bin/21_merge_two_sorted_lists.rs
@@ -1,67 +1,71 @@
 use std::borrow::{Borrow, BorrowMut};
+use std::ops::Deref;
 
 pub struct Solution {}
 
 // Definition for singly-linked list.
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
-  pub val: i32,
-  pub next: Option<Box<ListNode>>
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
 }
 
 impl ListNode {
-  #[inline]
-  fn new(val: i32) -> Self {
-    ListNode {
-      next: None,
-      val
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
     }
-  }
 }
 
 impl Solution {
-
-    pub fn merge_two_lists(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    pub fn merge_two_lists(
+        l1: Option<Box<ListNode>>,
+        l2: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
         let mut l1_iter = l1.borrow();
         let mut l2_iter = l2.borrow();
 
-        let &mut head_node = None.borrow_mut();
-        let &mut current_node = None.borrow_mut();
+        let mut head_node = None.borrow_mut();
+        let mut current_node = None.borrow_mut();
         while !l1_iter.is_none() || !l2_iter.is_none() {
-            let next_node = match (l1_iter, l2_iter) {
-                (None, None) => None.borrow_mut(),
-                (Some(l1_iter), None) =>
-                    Option::Some(Box::new(ListNode::new(l1_iter.val))).borrow_mut(),
-                (None, Some(l2_iter)) =>
-                    Option::Some(Box::new(ListNode::new(l2_iter.val))).borrow_mut(),
-                (Some(l1_iter), Some(l2_iter)) =>
-                    if l1_iter.val <= l2_iter.val {
-                        Option::Some(Box::new(ListNode::new(l1_iter.val)));
-                    } else {
-                        Option::Some(Box::new(ListNode::new(l1_iter.val)));
-                    }
-            };
-            match current_node {
-                None => {
-                    &head_node = current_node.borrow();
-                    &current_node = next_node.borrow();
+            let &mut next_val;
+            if let (Some(l1_val), None) = (l1_iter, l2_iter) {
+                next_val = l1_val.val;
+                l1_iter = l1_val.next.borrow();
+            } else if let (None, Some(l2_val)) = (l1_iter, l2_iter) {
+                next_val = l2_val.val;
+                l2_iter = l2_val.next.borrow();
+            } else if let (Some(l1_val), Some(l2_val)) = (l1_iter, l2_iter) {
+                if l1_val.val <= l2_val.val {
+                    next_val = l2_val.val;
+                    l2_iter = l2_val.next.borrow();
+                } else {
+                    next_val = l1_val.val;
+                    l1_iter = l1_val.next.borrow();
                 }
-                Some(&boxed_val) => {
-                    current_node.next = next_node;
-                    &current_node = next_node.borrow();
-                }
+            }
+            if current_node.is_none() {
+                current_node = &mut Option::Some(Box::from(ListNode::new(next_val)));
+                head_node = current_node;
+            } else {
+                let &mut next_node = &mut ListNode::new(next_val).borrow_mut();
+                next_node.next = current_node.clone();
+                current_node = Option::Some(Box::from(next_node));
             }
         }
 
-        return head_node;
+        return head_node.clone();
     }
-
 }
 
 fn main() {
-    println!("{}", Solution::merge_two_lists(
-        Option::Some(Box::new(ListNode::new(0))),
-        Option::Some(Box::new(ListNode::new(1))),
-    ).unwrap().val);
+    println!(
+        "{}",
+        Solution::merge_two_lists(
+            Option::Some(Box::new(ListNode::new(0))),
+            Option::Some(Box::new(ListNode::new(1))),
+        )
+        .unwrap()
+        .val
+    );
 }
-
